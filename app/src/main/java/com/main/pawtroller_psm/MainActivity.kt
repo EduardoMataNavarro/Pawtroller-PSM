ac96c39e348798dff5ef18d8adf7f8299339335b
Eduardo I. Mata Navarro <eduardo.matanavarro1998@gmail.com>
2020-10-23 22:27:04 -0500

    23-10-2020 - Added fragments and main mav

--- app/src/main/java/com/main/pawtroller_psm/MainActivity.kt
+++ app/src/main/java/com/main/pawtroller_psm/MainActivity.kt
@@ -2,10 +2,29 @@ package com.main.pawtroller_psm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_feed.setOnClickListener(){
+
+
+        }
+        btn_adopt.setOnClickListener(){
+
+        }
+        btn_pets.setOnClickListener(){
+
+        }
+        btn_forum.setOnClickListener(){
+
+        }
+        btn_profile.setOnClickListener(){
+
+        }
     }
 }
